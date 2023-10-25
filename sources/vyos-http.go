67849e4886f9c42401b4a1b8e99b4c928201a6aa
blacknon <blacknon@orebibou.com>
2023-10-25 17:30:08 +0900

    update.

--- sources/vyos-http.go
+++ sources/vyos-http.go
@@ -50,8 +50,10 @@ func (s *vyos) downloadImage(definition shared.Definition) error {
 	// https://legacy-lts-images.vyos.io/1.2.9-S1/vyos-1.2.9-S1-amd64.iso
 	// https://github.com/vyos/vyos-rolling-nightly-builds/releases/download/1.5-rolling-202310240118/vyos-1.5-rolling-202310240118-amd64.iso
 	// baseURL := s.definition.Source.URL
-	baseURL := "https://legacy-lts-images.vyos.io/1.2.9-S1/"
-	s.fname = "vyos-1.2.9-S1-amd64.iso"
+	// baseURL := "https://legacy-lts-images.vyos.io/1.2.9-S1/"
+	baseURL := "https://github.com/vyos/vyos-rolling-nightly-builds/releases/download/1.5-rolling-202310240118/"
+	// s.fname = "vyos-1.2.9-S1-amd64.iso"
+	s.fname = "vyos-1.5-rolling-202310240118-amd64.iso"
 
 	url, err := url.Parse(baseURL)
 	if err != nil {
